9f603df18e315861f6cf4583da791fec3bc450a4
kemochka <kemaevak@yandex.ru>
2023-12-01 18:33:30 +0300

    10. toString [#310018 #463598]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,13 +3,22 @@ package ru.job4j.tracker;
 import java.time.format.DateTimeFormatter;
 
 public class StartUI {
+    Item item = new Item();
+
+
     public static void main(String[] args) {
         Item item = new Item();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String createdFormat = formatter.format(item.getCreated());
         System.out.println(createdFormat);
         Item item1 = new Item();
-        String date = item1.toString();
-        System.out.println(date);
+        System.out.println(item1);
+    }
+
+    @Override
+    public String toString() {
+        return "StartUI{" +
+                "item=" + item +
+                '}';
     }
 }
