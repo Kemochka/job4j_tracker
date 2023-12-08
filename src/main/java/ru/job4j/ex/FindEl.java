c0312aeeb489e4d082d90f0e12446097e4ddec7f
kemochka <kemaevak@yandex.ru>
2023-12-08 19:38:31 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,26 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int index = 0; index < value.length; index ++) {
+            if (value[index].equals(key)) {
+                rsl = index;
+                break;
+            }
+        }
+        if (rsl == - 1) {
+            throw new ElementNotFoundException();
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String[] x = {"l", "h", "k"};
+        try {
+            indexOf(x, "m");
+        } catch (ElementNotFoundException exception) {
+            exception.fillInStackTrace();
+        }
+    }
+}
