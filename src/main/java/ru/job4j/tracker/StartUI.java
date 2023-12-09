51faf7442955dcad509b869b6e78bc06fcd55ab9
kemochka <kemaevak@yandex.ru>
2023-12-10 00:24:37 +0300

    1. Обеспечить бесперебойную работу приложения Tracker [#789]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,6 +3,7 @@ package ru.job4j.tracker;
 import ru.job4j.tracker.action.*;
 import ru.job4j.tracker.action.User;
 import ru.job4j.tracker.input.Input;
+import ru.job4j.tracker.input.Validate;
 import ru.job4j.tracker.output.Console;
 import ru.job4j.tracker.output.Output;
 
@@ -18,31 +19,36 @@ public class StartUI {
         while (run) {
             showMenu(actions);
             int select = input.askInt("Выбрать: ");
+            if (select < 0 || select >= actions.length) {
+                out.println("Неверный ввод, вы можете выбрать: 0 .. " + (actions.length - 1));
+                continue;
+            }
             User action = actions[select];
             run = action.execute(input, tracker);
-            }
-        }
-
-        private void showMenu(User[] actions) {
-            out.println("Меню:");
-            for (int index = 0; index < actions.length; index++) {
-                out.println(index + ". " + actions[index].name());
-            }
-        }
-
-        public static void main(String[] args) {
-            Output output = new Console();
-            Input input = new ru.job4j.tracker.input.Console();
-            Tracker tracker = new Tracker();
-            User[] actions = {
-                    new Create(output),
-                    new FindAll(output),
-                    new Replace(output),
-                    new Delete(output),
-                    new FindById(output),
-                    new FindByName(output),
-                    new Exit(output)
-            };
-            new StartUI(output).init(input, tracker, actions);
         }
     }
+
+
+    private void showMenu(User[] actions) {
+        out.println("Меню:");
+        for (int index = 0; index < actions.length; index++) {
+            out.println(index + ". " + actions[index].name());
+        }
+    }
+
+    public static void main(String[] args) {
+        Output output = new Console();
+        Input input = new Validate();
+        Tracker tracker = new Tracker();
+        User[] actions = {
+                new Create(output),
+                new FindAll(output),
+                new Replace(output),
+                new Delete(output),
+                new FindById(output),
+                new FindByName(output),
+                new Exit(output)
+        };
+        new StartUI(output).init(input, tracker, actions);
+    }
+}
