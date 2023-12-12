1f10bcdd4338e43f65b2e576e553aa7aa78d3aae
kemochka <kemaevak@yandex.ru>
2023-12-12 21:01:08 +0300

    1. Singleton [#94606]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -3,7 +3,10 @@ package ru.job4j.tracker;
 public class SingleTracker {
     private Tracker tracker = new Tracker();
     private static SingleTracker instance = null;
-    private SingleTracker() {}
+
+    private SingleTracker() {
+
+    }
 
     public static SingleTracker getInstance() {
         if (instance == null) {
