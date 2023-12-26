78f29c9ffb64a024cdaff6d40d9e1a22aa683365
kemochka <kemaevak@yandex.ru>
2023-12-26 17:02:03 +0300

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("kemaevak@yandex.ru", "Kemaeva Kristina Igorevna");
+        map.put("kkemaeva@list.ru", "Kris");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
