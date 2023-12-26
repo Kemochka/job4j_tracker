333e314d37c1aaa3dad567afa03ae9fedf978061
kemochka <kemaevak@yandex.ru>
2023-12-26 19:53:10 +0300

    2. Идентичные тексты [#212655 #468137]

--- src/main/java/ru/job4j/collection/UniqueText.java
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -4,7 +4,6 @@ import java.util.HashSet;
 
 public class UniqueText {
     public boolean isEquals(String originText, String duplicateText) {
-        boolean result = true;
         String[] origin = originText.split(" ");
         String[] text = duplicateText.split(" ");
         HashSet<String> check = new HashSet<>();
