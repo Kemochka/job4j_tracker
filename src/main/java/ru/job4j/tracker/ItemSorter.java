eab2960a1389f83801957572a800a6f8fd59f756
kemochka <kemaevak@yandex.ru>
2024-04-07 00:00:02 +0300

    2. Трекер SQL [#1734 #490224]

--- src/main/java/ru/job4j/tracker/ItemSorter.java
+++ src/main/java/ru/job4j/tracker/ItemSorter.java
@@ -1,14 +1,17 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
 
 public class ItemSorter {
     public static void main(String[] args) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         List<Item> items = Arrays.asList(
-                new Item(1, "Kris"),
-                new Item(2, "Victor"),
-                new Item(3, "Natali")
+                new Item(1, "Kris", LocalDateTime.parse("2024-04-06 23:34:33", formatter)),
+                new Item(2, "Victor", LocalDateTime.parse("2024-04-06 23:34:33", formatter)),
+                new Item(3, "Natali", LocalDateTime.parse("2024-04-06 23:34:33", formatter))
         );
         System.out.println(items);
         items.sort(new ItemAscByName());
