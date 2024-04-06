71864886a49f030abbdde57f981342689693af79
kemochka <kemaevak@yandex.ru>
2024-04-07 00:18:39 +0300

    2. Трекер SQL [#1734 #490224]

--- src/test/java/ru/job4j/tracker/ItemSorterTest.java
+++ src/test/java/ru/job4j/tracker/ItemSorterTest.java
@@ -1,6 +1,8 @@
 package ru.job4j.tracker;
 
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,15 +11,16 @@ class ItemSorterTest {
     @Test
     public void whenItemAscByName() {
         List<Item> items = Arrays.asList(
-                new Item(1, "Kris"),
-                new Item(2, "Victor"),
-                new Item(3, "Natali")
+                new Item(1, "Kris", LocalDateTime.parse("2024-04-06T23:34:33")),
+                new Item(2, "Victor", LocalDateTime.parse("2024-04-06T23:34:33")),
+                new Item(3, "Natali", LocalDateTime.parse("2024-04-06T23:34:33"))
         );
         items.sort(new ItemAscByName());
+
         List<Item> expected = Arrays.asList(
-                new Item(1, "Kris"),
-                new Item(3, "Natali"),
-                new Item(2, "Victor")
+                new Item(1, "Kris", LocalDateTime.parse("2024-04-06T23:34:33")),
+                new Item(3, "Natali", LocalDateTime.parse("2024-04-06T23:34:33")),
+                new Item(2, "Victor", LocalDateTime.parse("2024-04-06T23:34:33"))
         );
         assertThat(items).isEqualTo(expected);
     }
@@ -25,16 +28,18 @@ class ItemSorterTest {
     @Test
     public void whenItemDescByName() {
         List<Item> items = Arrays.asList(
-                new Item(1, "Kris"),
-                new Item(2, "Victor"),
-                new Item(3, "Natali")
+                new Item(1, "Kris", LocalDateTime.parse("2024-04-06T23:34:33")),
+                new Item(2, "Victor", LocalDateTime.parse("2024-04-06T23:34:33")),
+                new Item(3, "Natali", LocalDateTime.parse("2024-04-06T23:34:33"))
         );
         items.sort(new ItemDescByName());
+
         List<Item> expected = Arrays.asList(
-                new Item(2, "Victor"),
-                new Item(3, "Natali"),
-                new Item(1, "Kris")
+                new Item(2, "Victor", LocalDateTime.parse("2024-04-06T23:34:33")),
+                new Item(3, "Natali", LocalDateTime.parse("2024-04-06T23:34:33")),
+                new Item(1, "Kris", LocalDateTime.parse("2024-04-06T23:34:33"))
         );
         assertThat(items).isEqualTo(expected);
     }
+
 }
